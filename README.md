1fb289ae01671fc02d4e57cb85992b99119cc041
Richard Bramley <rabramley@gmail.com>
2021-11-02 14:08:03 +0000

    Academic fields not needed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# academics
+# Academics
+
+## Running Jobs
+
+### Redis Server
+
+To run redis, use the command:
+
+```
+redis-server
+```
--- migrations/versions/002__Create__Academic.py
+++ migrations/versions/002__Create__Academic.py
@@ -10,18 +10,6 @@ def upgrade(migrate_engine):
         "academic",
         meta,
         Column("id", Integer, primary_key=True),
-        Column("scopus_id", NVARCHAR(255)),
-        Column("eid", NVARCHAR(255)),
-        Column("first_name", NVARCHAR(500)),
-        Column("last_name", NVARCHAR(500)),
-        Column("affiliation_id", NVARCHAR(500)),
-        Column("affiliation_name", NVARCHAR(500)),
-        Column("affiliation_address", NVARCHAR(500)),
-        Column("affiliation_city", NVARCHAR(500)),
-        Column("affiliation_country", NVARCHAR(500)),
-        Column("citation_count", Integer),
-        Column("document_count", Integer),
-        Column("h_index", Integer),
         *get_audit_mixin_columns(),
     )
 
