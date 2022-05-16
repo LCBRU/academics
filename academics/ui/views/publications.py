1ffe4b25cc336bc9c1a9bc613145e48aa2ea179e
Richard Bramley <rabramlry@gmail.com>
2022-05-16 11:00:30 +0000

    Publication search

--- academics/ui/views/publications.py
+++ academics/ui/views/publications.py
@@ -3,12 +3,26 @@ from lbrc_flask.forms import SearchForm
 from academics.model import ScopusAuthor, ScopusPublication
 from .. import blueprint
 from sqlalchemy import or_
+from wtforms import SelectField
+
+
+def _get_author_choices():
+    return [('', '')] + [(a.id, a.full_name()) for a in ScopusAuthor.query.order_by(ScopusAuthor.last_name, ScopusAuthor.first_name).all()]]
+
+
+class TrackerSearchForm(SearchForm):
+    author = SelectField('RAG Rating', choices=[])
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.author.choices = _get_author_choices()
 
 
 @blueprint.route("/publications/")
 @blueprint.route("/author/<int:author_id>/publications/")
 def publications(author_id=None):
-    search_form = SearchForm(formdata=request.args)
+    search_form = TrackerSearchForm(formdata=request.args)
     
     q = ScopusPublication.query
 
