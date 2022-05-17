2ef442acbea27d6e4b028ffe70981af51f98b34b
Richard Bramley <rabramlry@gmail.com>
2022-05-17 09:52:53 +0000

    Edit academics link

--- academics/ui/views/academics.py
+++ academics/ui/views/academics.py
@@ -27,7 +27,7 @@ class AddAuthorForm(FlashingForm):
 
 
 def _get_theme_id_choices():
-    return [('', '')] + [(t.id, t.name) for t in Theme.all()]
+    return [('', '')] + [(t.id, t.name) for t in Theme.query.all()]
 
 
 class AcademicEditForm(FlashingForm):
