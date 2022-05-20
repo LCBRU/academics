0e03b0e06ad0755299370ed306b204f231758028
Richard Bramley <rabramlry@gmail.com>
2022-05-20 12:55:26 +0000

    Add orcid to author

--- migrations/versions/012__Update__Academic__add__theme.py
+++ migrations/versions/012__Update__Academic__add__theme.py
@@ -1,5 +1,5 @@
 from operator import index
-from sqlalchemy import ForeignKey, Integer, MetaData, Table, Column, NVARCHAR
+from sqlalchemy import ForeignKey, Integer, MetaData, Table, Column
 from sqlalchemy import Index
 
 
@@ -8,14 +8,18 @@ meta = MetaData()
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
 
+    th = Table("theme", meta, autoload=True)
     t = Table("academic", meta, autoload=True)
 
-    orcid = Column("orcid", NVARCHAR(50))
-    orcid.create(t)
+    theme_id = Column("theme_id", Integer, ForeignKey(th.c.id))
+    theme_id.create(t)
+
+    idx = Index('ix__academic__theme_id', t.c.theme_id)
+    idx.create(migrate_engine)
 
 
 def downgrade(migrate_engine):
     meta.bind = migrate_engine
     t = Table("academic", meta, autoload=True)
 
-    t.c.orcid.drop()
+    t.c.theme_id.drop()
