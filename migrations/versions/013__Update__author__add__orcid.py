5918f03d235d9b516052dfaae550ffc03e0447e7
Richard Bramley <rabramlry@gmail.com>
2022-05-20 13:00:18 +0000

    Add orcid to author

---
+++ migrations/versions/013__Update__author__add__orcid.py
@@ -0,0 +1,21 @@
+from operator import index
+from sqlalchemy import ForeignKey, Integer, MetaData, Table, Column, NVARCHAR
+from sqlalchemy import Index
+
+
+meta = MetaData()
+
+def upgrade(migrate_engine):
+    meta.bind = migrate_engine
+
+    t = Table("author", meta, autoload=True)
+
+    orcid = Column("orcid", NVARCHAR(50))
+    orcid.create(t)
+
+
+def downgrade(migrate_engine):
+    meta.bind = migrate_engine
+    t = Table("author", meta, autoload=True)
+
+    t.c.orcid.drop()
