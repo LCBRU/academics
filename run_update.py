386b6fb6993b3bdf8b753854673ed23ee7784cf2
Richard Bramley <rabramlry@gmail.com>
2022-09-15 11:58:14 +0000

    Manually update academics

---
+++ run_update.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from '.env' file.
+load_dotenv()
+
+from academics import create_app
+from academics.scopus.service import _update_all_academics
+
+application = create_app()
+application.app_context().push()
+
+application.config['SERVER_NAME'] = os.environ["CELERY_SERVER_NAME"]
+
+_update_all_academics()
