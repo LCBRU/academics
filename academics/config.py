d960a9bc5ecabeee9b1ed33405f1fd40884e7603
Richard Bramley <rabramley@gmail.com>
2023-08-31 15:02:10 +0100

    Parse historic date

--- academics/config.py
+++ academics/config.py
@@ -1,11 +1,12 @@
 import os
 from lbrc_flask.config import BaseConfig, BaseTestConfig
+from lbrc_flask.validators import parse_date
 
 
 class Config(BaseConfig):
     SCOPUS_API_KEY = os.environ["SCOPUS_API_KEY"]
     OPEN_ALEX_EMAIL = os.environ["OPEN_ALEX_EMAIL"]
-    HISTORIC_PUBLICATION_CUTOFF = os.environ["HISTORIC_PUBLICATION_CUTOFF"]
+    HISTORIC_PUBLICATION_CUTOFF = parse_date(os.environ["HISTORIC_PUBLICATION_CUTOFF"])
 
 
 class TestConfig(BaseTestConfig):
