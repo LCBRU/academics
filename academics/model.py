3852868c09573aaef9be466dcae0670e61973b3d
Richard Bramley <rabramley@gmail.com>
2023-09-15 17:30:04 +0100

    Add sources

--- academics/model.py
+++ academics/model.py
@@ -1,8 +1,8 @@
+import logging
 from lbrc_flask.security import AuditMixin
 from lbrc_flask.model import CommonMixin
 from lbrc_flask.database import db
 from lbrc_flask.security import User as BaseUser
-from itertools import chain
 
 from sqlalchemy import func, select
 
@@ -47,13 +47,19 @@ class Academic(AuditMixin, CommonMixin, db.Model):
         )
 
     def set_name(self):
+        logging.info('1')
         if not self.sources:
+            logging.info('2')
             return
 
+        logging.info('3')
         top_author = sorted(self.sources, key=lambda a: a.document_count, reverse=True)[0]
 
+        logging.info('4')
         self.first_name = top_author.first_name
+        logging.info('5')
         self.last_name = top_author.last_name
+        logging.info('6')
 
     @property
     def orcid_link(self):
