a4cf3376b26f6dda480f2121ee0e4ff027a188f9
Richard Bramley <rabramley@gmail.com>
2023-10-13 17:42:28 +0100

    Limit by month

--- new_publication_notification.py
+++ new_publication_notification.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+from datetime import date
+from dateutil.relativedelta import relativedelta 
 from dotenv import load_dotenv
 from flask import render_template
 from sqlalchemy import select
 from lbrc_flask.database import db
 from lbrc_flask.emailing import email
+from lbrc_flask.security import get_users_for_role
+from academics.security import ROLE_NEW_PUBLICATION_RECIPIENT
 
 from academics.model import ScopusPublication
 from academics import create_app
@@ -14,14 +18,25 @@ load_dotenv()
 application = create_app()
 application.app_context().push()
 
-q = select(ScopusPublication)
+today = date.today()
+this_month_start = date(today.year, today.month, 1)
+last_month_start = this_month_start - relativedelta(months=1)
+
+q = (
+    select(ScopusPublication)
+    .where(ScopusPublication.created_date.between(this_month_start, last_month_start))
+)
 
 publications = list(db.session.execute(q).unique().scalars())
 
-email(
-    subject='New Publications this Month',
-    message=render_template('email/new_publications.txt', publications=publications),
-    recipients=[],
-    html_template='email/new_publications.html',
-    publications=publications,
-)
+recipients = get_users_for_role(ROLE_NEW_PUBLICATION_RECIPIENT)
+
+print(len(publications))
+
+# email(
+#     subject='New Publications this Month',
+#     message=render_template('email/new_publications.txt', publications=publications),
+#     recipients=[],
+#     html_template='email/new_publications.html',
+#     publications=publications,
+# )
