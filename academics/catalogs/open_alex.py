2b93e06f38354474cf0642f63093487dc3f22b4b
Richard Bramley <rabramley@gmail.com>
2023-10-30 14:27:31 +0000

    Wrong search criteria

--- academics/catalogs/open_alex.py
+++ academics/catalogs/open_alex.py
@@ -52,7 +52,7 @@ def open_alex_similar_authors(academic: Academic):
     return []
 
 def _get_for_orcid(orcid):
-    q = Authors().filter(scopus=orcid)
+    q = Authors().filter(orcid=orcid)
     return chain(*q.paginate(per_page=200))
 
 
