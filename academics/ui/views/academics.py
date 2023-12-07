933fa20e233cdd6bdf223f6255e73a7f5d502623
Richard Bramley <rabramley@gmail.com>
2023-12-07 13:00:56 +0000

    Academic ordering

--- academics/ui/views/academics.py
+++ academics/ui/views/academics.py
@@ -68,7 +68,7 @@ def index():
         else:
             q = q.filter(Academic.theme_id == search_form.theme_id.data)
     
-    q.order_by(Academic.last_name).orber_by(Academic.first_name)
+    q.order_by(Academic.last_name).order_by(Academic.first_name)
 
     academics = q.paginate(
         page=search_form.page.data,
