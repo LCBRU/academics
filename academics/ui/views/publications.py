d5295c1edcdacfc40e072f88252c4f893d1abaa2
Richard Bramley <rabramley@gmail.com>
2023-12-12 12:23:48 +0000

    Debugging

--- academics/ui/views/publications.py
+++ academics/ui/views/publications.py
@@ -178,6 +178,8 @@ def publication_full_annual_report_xlsx():
 
     print('Gonna query')
 
+    print(q)
+
     publication_details = ({
         'Publication Reference': p.vancouverish,
         'DOI': p.doi,
