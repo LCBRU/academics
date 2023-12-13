b4c0c9292afe0756129683235c748fb34265693e
Richard Bramley <rabramley@gmail.com>
2023-12-13 12:45:41 +0000

    Debugging

--- academics/ui/views/publications.py
+++ academics/ui/views/publications.py
@@ -168,7 +168,7 @@ def publication_full_export_xlsx():
     # )
 
     q = publication_search_query(search_form)
-    q = q.with_only_columns([CatalogPublication.id])
+    q = q.with_only_columns(CatalogPublication.id)
 
     logging.getLogger('query').info(q)
     # logging.info(list(q.inner_columns))
