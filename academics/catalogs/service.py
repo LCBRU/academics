8770be6ead6b7b6ca5ca13accc79f89ade623ac1
Richard Bramley <rabramley@gmail.com>
2023-12-19 12:20:26 +0000

    Debug authors

--- academics/catalogs/service.py
+++ academics/catalogs/service.py
@@ -478,7 +478,7 @@ def add_catalog_publications(catalog, publication_datas):
 
     existing_cat_ids = {db.session.execute(q).scalars()}
 
-    print(list(existing_cat_ids))
+    print([p for p in existing_cat_ids])
     new_pubs = [p for p in publication_datas if p.catalog_identifier not in existing_cat_ids]
 
     save_publications(catalog, new_pubs)
