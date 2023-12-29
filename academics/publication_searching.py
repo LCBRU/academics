6ddb69d951c595815aa0c82023c9615c940bad63
Richard Bramley <rabramley@gmail.com>
2023-12-29 15:24:58 +0000

    Switch to catalog publication sources

--- academics/publication_searching.py
+++ academics/publication_searching.py
@@ -131,6 +131,10 @@ def publication_search_query(search_form):
 
     bcp = best_catalog_publications()
 
+    logging.getLogger('query').warn(bcp)
+
+    return None
+
     q = select(CatalogPublication).select_from(bcp).join(CatalogPublication.publication)
 
     logging.getLogger('query').warn(q)
