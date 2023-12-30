ece4ff76a3bbbb18223073061cdc3c3919581ca1
Richard Bramley <rabramley@gmail.com>
2023-12-30 12:47:20 +0000

    Catalog publication keywords

--- academics/catalogs/service.py
+++ academics/catalogs/service.py
@@ -601,6 +601,9 @@ def save_publications(new_pubs):
         cat_pub.cited_by_count = p.cited_by_count
         cat_pub.journal_id = journal_xref[cpr]
         cat_pub.subtype_id = subtype_xref[cpr]
+
+        print(sponsor_xref[cpr])
+
         cat_pub.sponsors = set(sponsor_xref[cpr])
         cat_pub.keywords = set(keyword_xref[cpr])
 
