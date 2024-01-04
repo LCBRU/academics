bdd161880e172593b32728d13da0465214443590
Richard Bramley <rabramley@gmail.com>
2024-01-04 16:01:29 +0000

    Debug names

--- academics/catalogs/scopus.py
+++ academics/catalogs/scopus.py
@@ -255,7 +255,7 @@ def scopus_author_search(search_string):
     else:
         q = f'AUTHLASTNAME({search_string})'
 
-    auth_srch = ElsSearch(q,'author')
+    auth_srch = ElsSearch(f'{q} AND affil(leicester)','author')
     auth_srch.execute(_client(), get_all=True)
 
     existing_catalog_identifiers = set(db.session.execute(
