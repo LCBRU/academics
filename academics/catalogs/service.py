8f2bc07edc8445a52e904626358444dc3df52af8
Richard Bramley <rabramley@gmail.com>
2024-01-26 17:00:52 +0000

    Refresh publications

--- academics/catalogs/service.py
+++ academics/catalogs/service.py
@@ -234,7 +234,11 @@ def refresh_publications():
                     save_publications([pub_data])
                     logging.info(pub_data)
 
-            if not p.scopus_catalog_publication and not p.institutions:
+            logging.warning(p.scopus_catalog_publication)
+            logging.warning(not p.institutions)
+            logging.warning(p.scopus_catalog_publication and not p.institutions)
+
+            if p.scopus_catalog_publication and not p.institutions:
                 logging.warning("I'm doing it")
                 get_scival_publication_data(p.scopus_catalog_publication.catalog_identifier)
 
