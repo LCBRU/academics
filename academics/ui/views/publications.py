86f02f43bf73c25b44478ba8ec3797bd984af4e2
Richard Bramley <rabramley@gmail.com>
2024-02-01 10:00:51 +0000

    Search for industrial collaboration

--- academics/ui/views/publications.py
+++ academics/ui/views/publications.py
@@ -51,8 +51,10 @@ def publications():
     q = q.order_by(CatalogPublication.publication_cover_date.desc())
 
     print('A'*10)
-    print(db.session.execute(select(func.count()).select_from(q)).scalar())
+    print(q)
     print('B'*10)
+    print(db.session.execute(select(func.count()).select_from(q)).scalar())
+    print('C'*10)
 
     publications = db.paginate(
         select=q,
