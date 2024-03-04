09599c410fd3aabe72c6adbe9849d5625cf73de1
Richard Bramley <rabramley@gmail.com>
2024-03-04 13:31:54 +0000

    Need a context

--- scopus_test.py
+++ scopus_test.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 from dotenv import load_dotenv
+from academics import create_app
 
 # Load environment variables from '.env' file.
 load_dotenv()
 
 from academics.catalogs.scopus import get_scopus_publication_data
+
+application = create_app()
+application.app_context().push()
 
 out = get_scopus_publication_data(doi='10.1186/s12872-023-03623-y')
 
