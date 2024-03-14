3d93a688bfdb5e710f885c2b579795028164c20d
Richard Bramley <rabramley@gmail.com>
2024-03-14 14:31:23 +0000

    Do the thing

--- academics/catalogs/service.py
+++ academics/catalogs/service.py
@@ -46,13 +46,13 @@ def auto_validate():
         select(Publication)
         .where(Publication.nihr_acknowledgement_id == None)
         .where(Publication.auto_nihr_acknowledgement_id == None)
-        .where(Publication.catalog_publications.any(CatalogPublication.subtype_id.has(Subtype.id.in_(validation_type_ids))))
+        .join(Publication.catalog_publications)
+        .where(CatalogPublication.subtype.has(Subtype.id.in_(validation_type_ids)))
     )
 
     print(q)
 
     for p, i in enumerate(db.session.execute(q).unique().scalars().all()):
-        pass
         # auto_ack = _get_nihr_acknowledgement(p)
 
         # if auto_ack:
