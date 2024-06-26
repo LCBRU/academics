111b3febe2891902b991419c6239c6e14f93814b
Richard Bramley <rabramley@gmail.com>
2024-06-26 16:00:03 +0100

    Show all publications from jan 1st

---
+++ scopus_date_check.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from datetime import date
+from dotenv import load_dotenv
+from flask import current_app
+from academics import create_app
+from sqlalchemy import select
+from lbrc_flask.database import db
+
+from academics.model.publication import CatalogPublication
+
+# Load environment variables from '.env' file.
+load_dotenv()
+
+from academics.catalogs.scopus import ScopusClient, get_scopus_publication_data
+
+application = create_app()
+application.app_context().push()
+
+# out = get_scopus_publication_data(doi=sys.argv[1], log_data=True)
+
+client = ScopusClient(current_app.config['SCOPUS_API_KEY'])
+
+q = select(CatalogPublication).where(CatalogPublication.publication_cover_date == date(day=1, month=1, year=2023))
+
+for p in list(db.session.execute(q).scalars())[:1]:
+    uri=f'https://api.elsevier.com/content/abstract/doi/{p.doi}'
+
+    # resp = client.exec_request(uri)
+
+    print(uri)
