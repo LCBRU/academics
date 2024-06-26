3460e656e2866abbed86c6fc232dce431f8168e0
Richard Bramley <rabramley@gmail.com>
2024-06-26 16:52:49 +0100

    Show all publications from jan 1st

--- scopus_date_check.py
+++ scopus_date_check.py
@@ -23,7 +23,7 @@ client = ScopusClient(current_app.config['SCOPUS_API_KEY'])
 
 q = select(CatalogPublication).where(CatalogPublication.publication_cover_date == date(day=1, month=1, year=2023))
 
-for p in list(db.session.execute(q).scalars())[:1]:
+for p in list(db.session.execute(q).scalars()):
     uri=f'https://api.elsevier.com/content/abstract/doi/{p.doi}'
 
     # resp = client.exec_request(uri)
