2c600c5ed6279fc893dba535a83daa045b7d48d9
Richard Bramley <rabramley@gmail.com>
2024-10-22 11:54:35 +0100

    Add a job to run the jobs - in case the original job is killed

---
+++ run_jobs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+from lbrc_flask.database import db
+
+# Load environment variables from '.env' file.
+from dotenv import load_dotenv
+load_dotenv()
+
+from academics.catalogs.jobs import RefreshAll
+
+from academics import create_app
+from lbrc_flask.async_jobs import AsyncJobs, run_jobs_asynch
+
+application = create_app()
+application.app_context().push()
+
+application.config['SERVER_NAME'] = os.environ["CELERY_SERVER_NAME"]
+
+run_jobs_asynch()
