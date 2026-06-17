46a1607d22fa87dc3bccd3402e09ab569512f837
Richard Bramley <rabramley@gmail.com>
2026-06-17 15:43:21 +0100

    Squish more exceptions

--- celery_worker.py
+++ celery_worker.py
@@ -3,6 +3,7 @@
 import os
 from dotenv import load_dotenv
 import urllib3
+from flask import g
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -13,6 +14,8 @@ from academics import create_app
 
 application = create_app()
 application.app_context().push()
+g.lbrc_flask_title = 'Academics Celery Worker'
+
 
 application.config['SERVER_NAME'] = os.environ["CELERY_SERVER_NAME"]
 
