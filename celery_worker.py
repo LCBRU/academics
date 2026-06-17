6d659532b0946dc1cc505d9e08919b4dee6ada19
Richard Bramley <rabramley@gmail.com>
2026-06-17 15:47:09 +0100

    Update requirements

--- celery_worker.py
+++ celery_worker.py
@@ -3,7 +3,6 @@
 import os
 from dotenv import load_dotenv
 import urllib3
-from flask import g
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -14,9 +13,8 @@ from academics import create_app
 
 application = create_app()
 application.app_context().push()
-g.lbrc_flask_title = 'Academics Celery Worker'
 
-
+application.config['APP_NAME'] = "Academics Celery Worker"
 application.config['SERVER_NAME'] = os.environ["CELERY_SERVER_NAME"]
 
 from lbrc_flask.celery import celery
